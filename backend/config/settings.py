70b584d7130fe524577c8e5bbbcb3d137653e389
ganasaipunja <ganasaisai624@gmail.com>
2026-03-03 21:35:40 +0530

    Security: Moved email settings to environment variables change

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,16 +93,16 @@ else:
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 AUTH_USER_MODEL = 'api.User'
 
-EMAIL_TIMEOUT = 20
+EMAIL_TIMEOUT = 30
 
 # Static and Media files settings
 STATIC_URL = '/static/'

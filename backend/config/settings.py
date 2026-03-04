405624e0aad108cfc5ea6e82e40ffc553fddae70
ganasaipunja <ganasaisai624@gmail.com>
2026-03-04 18:37:50 +0530

    Change Brevo SMTP to port 465 (SSL) to avoid Render timeouts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,9 +97,9 @@ else:
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_API_KEY') 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
